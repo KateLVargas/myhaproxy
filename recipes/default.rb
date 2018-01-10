23fd6ac7156dc2c0f9377bf8628c2837d6a6f182
KateLVargas <kathryn.l.vargas@gmail.com>
2018-01-10 10:00:38 -0500

    Initial Upload

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+
+  node.default['haproxy']['members'] =
+  
+  [{
+    "hostname" => "ec2-54-146-48-69.compute-1.amazonaws.com",
+    "ipaddress" => "54.146.48.69",
+    "port" => 80,
+    "ssl_port" => 80
+  }]
+  
+  include_recipe 'haproxy::manual'
