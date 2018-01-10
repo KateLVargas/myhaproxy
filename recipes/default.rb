418d2ca1809433871b90282a9880471b33e19ffc
KateLVargas <kathryn.l.vargas@gmail.com>
2018-01-10 12:53:16 -0500

    LB myiis and apache_web working

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,12 @@
     "ipaddress" => "54.146.48.69",
     "port" => 80,
     "ssl_port" => 80
+  },
+   {
+    "hostname" => "ec2-52-206-230-214.compute-1.amazonaws.com",
+    "ipaddress" => "52.206.230.214",
+    "port" => 80,
+    "ssl_port" => 80
   }]
   
   include_recipe 'haproxy::manual'
